309110ef3f4ee5ad834bdb3ba09d708728dbed24
Niall Maher <niall@codu.ie>
2021-03-06 13:52:46 +0000

    Update meta description

--- components/meta.js
+++ components/meta.js
@@ -41,7 +41,8 @@ export default function Meta() {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={`Exploring Ideas on Modern Web
+        Development and Technology Business.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
