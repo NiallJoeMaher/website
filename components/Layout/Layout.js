07b8caefec2df555a36529c082796764201ed765
Niall Maher <niall@codu.ie>
2021-03-07 15:10:23 +0000

    fixes overflow problem with orb on blog post

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -4,26 +4,25 @@ import { Nav, Footer } from "../../components";
 
 export default function Layout({ children, absoluteOrbs }) {
   return (
-    <div className="relative bg-gray-900 overflow-y-hidden">
+    <div className="relative bg-gray-900 overflow-y-hidden max-w-100">
       <div
         className={`${
           absoluteOrbs ? "absolute" : "fixed"
         } h-56 w-56 -left-32 top-36 md:h-96 md:w-96 md:-left-24 bg-contain`}
         style={{ backgroundImage: 'url("/images/elipse.png")' }}
       />
-      <div
-        className={`${
-          absoluteOrbs ? "absolute" : "fixed"
-        } h-56 w-56 -right-32 md:h-96 md:w-96 md:-right-36 top-16 bg-contain`}
-        style={{ backgroundImage: 'url("/images/elipse.png")' }}
-      />
+      <div className="absolute left-0 right-0 bottom-0 top-0 overflow-y-hidden">
+        <div
+          className={`${
+            absoluteOrbs ? "absolute" : "fixed"
+          } h-56 w-56 -right-32 md:h-96 md:w-96 md:-right-36 top-16 bg-contain`}
+          style={{ backgroundImage: 'url("/images/elipse.png")' }}
+        />
+      </div>
       <Meta />
       <div className="relative">
-        <div className="relative">
-          <div />
-          <Nav />
-          <main className="max-w-5xl mx-auto">{children}</main>
-        </div>
+        <Nav />
+        <main className="max-w-5xl mx-auto">{children}</main>
       </div>
       <Footer />
     </div>
