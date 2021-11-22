1097eb4a6902a23beaffe81ec236f211e96e2a9c
Niall Maher <niall@codu.ie>
2021-11-22 01:37:07 +0000

    fixes typo in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    constent: ["./components/**/*.js", "./pages/**/*.js"],
+    content: ["./components/**/*.js", "./pages/**/*.js"],
     purge: {
       safelist: [
         "pink-600",
